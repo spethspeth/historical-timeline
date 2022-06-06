d58ecfd0cd55f2c2c32a75cfe35850e08efdf269
Stephen Postma <stephensebastiaanpostma@gmail.com>
2022-06-06 17:08:30 +0200

    made basic models

---
+++ db/migrate/20220606145218_add_user_to_timelines.rb
@@ -0,0 +1,5 @@
+class AddUserToTimelines < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :timelines, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_135412) do
+ActiveRecord::Schema.define(version: 2022_06_06_145412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.bigint "timeline_id", null: false
+    t.index ["timeline_id"], name: "index_bookmarks_on_timeline_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "timelines", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_timelines_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +58,8 @@ ActiveRecord::Schema.define(version: 2022_06_06_135412) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookmarks", "timelines"
+  add_foreign_key "bookmarks", "users"
+  add_foreign_key "events", "users"
+  add_foreign_key "timelines", "users"
 end
