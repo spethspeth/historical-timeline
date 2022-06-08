bae9fc1b66080470797921a76635fecc682f38ae
Adrien Larbouillat <ala@acoservices.eu>
2022-06-08 16:18:08 +0200

    create eventstimeline on timeline edit page done

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -29,11 +29,14 @@ class TimelinesController < ApplicationController
   end
 
   def edit
+    @events = Event.all
+    @eventstimeline = EventsTimeline.new
   end
 
   def update
     @timeline.update(timeline_params)
     @timeline.save ? (redirect_to timeline_path(@timeline)) : (render :edit)
+    raise
   end
 
   def destroy
