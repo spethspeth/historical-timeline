80725a19262ca1a6443652983a4d916f32cbb6f9
Grazia Cabras <graziacabras.gcabras@gmail.com>
2022-06-15 10:43:16 +0200

    fix little bug for destroy timeline

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -2,13 +2,13 @@ class Timeline < ApplicationRecord
   include PgSearch::Model
 
   belongs_to :user
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :destroy
   has_many :events_timelines, dependent: :destroy
   has_many :events, through: :events_timelines
   has_many :users, through: :bookmarks
   has_one_attached :photo
   has_many :reviews, dependent: :destroy
-  has_many :eras_timelines
+  has_many :eras_timelines, dependent: :destroy
   has_many :eras, through: :eras_timelines
 
   pg_search_scope :timeline_search,
