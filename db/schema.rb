3063460ff442567ce702729a8ef417deef852b62
Grazia Cabras <graziacabras.gcabras@gmail.com>
2022-06-16 09:22:47 +0200

    Fixing some styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_14_085331) do
+ActiveRecord::Schema.define(version: 2022_06_15_190117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,7 +94,6 @@ ActiveRecord::Schema.define(version: 2022_06_14_085331) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "comment"
-    t.integer "rating"
     t.bigint "user_id", null: false
     t.bigint "timeline_id"
     t.datetime "created_at", precision: 6, null: false
