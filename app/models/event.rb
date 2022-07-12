b385a4e85762ae1eed4430b351df820ca8034eb2
Stephen Sebastiaan Postma <stephensebastiaanpostma@gmail.com>
2022-07-12 11:22:51 +0200

    forms working and validations added for day month and year

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,5 +7,13 @@ class Event < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
+  validates :start_month, inclusion: { in: 1..12, allow_nil: true }
+  validates :end_month, inclusion: { in: 1..12, allow_nil: true }
+
+  # start_year is always present, the others are conditional, but month should be present if day is present, and so on
   validates :start_year, presence: true
+
+  validates :start_month, presence: { message: "Cannot assign start day without start month" }, if: :start_day?
+  validates :end_month, presence: { message: "Cannot assign end day without end month" }, if: :end_day?
+  validates :end_year, presence: { message: "Cannot assign end month without end year" }, if: :end_month?
 end
